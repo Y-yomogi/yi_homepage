2ec3840225057fa51c5d29d3fb883b395a9e8b59
Y-yomogi <Y-yomogi@users.noreply.github.com>
2022-07-01 04:10:04 +0900

    初期コミット

---
+++ src/ts/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        test
+      </header>
+    </div>
+  );
+}
+
+export default App;
